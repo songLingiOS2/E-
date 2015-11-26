60857da2053be095c22911064bc939f77bc6fa7b
yd <yaode@yddeiMac.local>
2015-11-26 08:52:18 +0800

    Initial Commit

---
+++ E校园/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  E校园
+//
+//  Created by yd on 15/11/26.
+//  Copyright (c) 2015年 yd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
